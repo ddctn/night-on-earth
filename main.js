722ca8fa87af5bd207658c8c8c3984400cf1d77b
Eva <avociafuture@gmail.com>
2025-10-15 02:47:58 +0300

    probably final version

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 "use strict"
+
+const btn = document.getElementById('translate-btn');
+let lang = true;
+const firstname = document.getElementById('firstName');
+const lastname = document.getElementById('lastName');
+
+function translate() {
+  if(lang === true){
+    firstname.innerText = 'Evelina'
+    lastname.innerText = 'Alieva'
+    btn.innerText = 'перевести на русский'
+    lang = false;
+  } else {
+    firstname.innerText = 'Эвелина'
+    lastname.innerText = 'Алиева'
+    btn.innerText = 'перевести на английский'
+    lang = true;
+  }
+}
+
+btn.addEventListener('click', translate);
