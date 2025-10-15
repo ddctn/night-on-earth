ce25b8fd7ec81b43b43b4f1b53866019245c711f
Eva <avociafuture@gmail.com>
2025-10-15 10:41:17 +0300

    address added

--- task.js
+++ task.js
@@ -7,14 +7,12 @@ const result = document.getElementById('result');
 const err = document.getElementById('error');
 const form = document.getElementById('task-form');
 
-
 const verifyBtn = document.getElementById('check');
 const submitBtn = document.getElementById('send');
 let check = false;
 
 verifyBtn.addEventListener('click', verify);
 submitBtn.addEventListener('click', sendForm);
-
 
 function verify() {
   let a = parseInt(elementA.value);
@@ -23,12 +21,12 @@ function verify() {
   let m = parseInt(elementM.value);
   let k = parseInt(elementK.value);
 
-  if(((m - a) >= 0 && (k - b) >= 0) || 
+  if(((m - a) >= 0 && (k - b) >= 0) ||
      ((m - a) >= 0 && (k - c) >= 0) ||
-     ((m - b) >= 0 && (k - a) >= 0) || 
+     ((m - b) >= 0 && (k - a) >= 0) ||
      ((m - b) >= 0 && (k - c) >= 0) ||
-     ((m - c) >= 0 && (k - a) >= 0) || 
-     ((m - c) >= 0 && (k - b) >= 0) 
+     ((m - c) >= 0 && (k - a) >= 0) ||
+     ((m - c) >= 0 && (k - b) >= 0)
     ){
     check = true;
     result.innerHTML = 'Коробка пройдет в дверь';
